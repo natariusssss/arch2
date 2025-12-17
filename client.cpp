848a80060888b904d566f2a4c333ec0965d0147e
Ксения Евдокимова <ksuuu2711@gmail.com>
2025-12-17 16:57:31 +0300

    update client.cpp

--- client.cpp
+++ client.cpp
@@ -5,57 +5,42 @@
 #include <unistd.h>
 #include <cstring>
 #include <string>
-
+#include "utils.h"
 int main() {
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (clientSocket < 0) {
-        std::cerr << "Error creating socket" << std::endl;
-        return 1;
+        die("Error creating socket");
     }
-
     sockaddr_in serverAddr{};
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(1111);
     if (inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr) <= 0) {
-        std::cerr << "Invalid address" << std::endl;
         close(clientSocket);
-        return 1;
+        die("Invalid address");
     }
-
     if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        std::cerr << "Connection failed" << std::endl;
         close(clientSocket);
-        return 1;
+        die("Connection failed");
     }
-
     std::cout << "Connected to server. Type messages (type 'exit' to quit):" << std::endl;
-
     std::string message;
     while (true) {
         std::cout << ">>> ";
-        std::getline(std::cin, message);
-
-
+        if (!std::getline(std::cin, message)) break;
         if (send(clientSocket, message.c_str(), message.length(), 0) < 0) {
             std::cerr << "Send failed" << std::endl;
             break;
         }
-
-
         if (message == "exit") {
             std::cout << "Exiting..." << std::endl;
             break;
         }
-
-
         char buffer[1024] = {0};
         ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-
         if (bytesReceived <= 0) {
-            std::cout << "Server closed connection or error occurred." << std::endl;
+            std::cout << "Server closed connection or timeout occurred." << std::endl;
             break;
         }
-
         buffer[bytesReceived] = '\0';
         std::cout << "[Server]: " << buffer << std::endl;
     }
